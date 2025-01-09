4a2c4c7f10e9650f35d3d7521db3fcc32e69e63b
gmnx <azzam.cyber@gmail.com>
2025-01-09 16:16:41 +0700

    fix: using registry.pintarai.com registry

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@
 # This script builds the mardi-ai-engine docker container from source.
 #
 
-CONTAINER_LOCAL_IMAGE="pintarai/mardi:latest"
+CONTAINER_LOCAL_IMAGE="registry.pintarai.com/pintarai/mardi:latest"
 echo "CONTAINER_LOCAL_IMAGE=$CONTAINER_LOCAL_IMAGE"
 		
 # build the container
 sudo docker build -t $CONTAINER_LOCAL_IMAGE -f Dockerfile .
+sudo docker push $CONTAINER_LOCAL_IMAGE
